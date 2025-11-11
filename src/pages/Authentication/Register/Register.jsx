c44de506fde0474da114d939dfee9ce5a81aa7b8
shakil <shakil.hossain450s@gmail.com>
2025-11-11 18:27:41 +0600

    get signin and register form data using react hook form

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,43 +1,71 @@
-import React from 'react';
 import PrimaryButton from '../../../components/PrimaryButton';
 import { Link } from 'react-router';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const handleRegister = (data) => {
+    console.log(data);
+    console.log(errors);
+  }
+
   return (
     <section className='max-w-sm'>
       <h2 className='text-4xl font-bold mb-3'>Create an account</h2>
       <p>Register with ZapShift</p>
-      <form className='mt-4 space-y-3'>
+      <form onSubmit={handleSubmit(handleRegister)} className='mt-4 space-y-3'>
         {/* name */}
         <div>
           <label htmlFor='name' className='text-[#0F172AFF]'>Name *</label>
           <input
             type='text'
-            name='name'
+            {...register('name', {
+              required: true,
+            })}
             placeholder='Name'
             className='border mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
           />
+          {errors.name?.type === 'required' && <p className='text-red-500'>name is required!</p>}
         </div>
         {/* email */}
         <div>
           <label htmlFor='email' className='text-[#0F172AFF]'>Email *</label>
           <input
             type='email'
-            name='email'
+            {...register('email', {
+              required: true,
+            })}
             placeholder='Email'
             className='border mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
           />
+          {errors.email?.type === 'required' && <p className='text-red-500'>email is required!</p>}
         </div>
         {/* password */}
         <div>
           <label htmlFor='password' className='text-[#0F172AFF]'>Password *</label>
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            className='border mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
-          />
+          <div className='relative'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              {...register('password', {
+                required: true,
+                minLength: 6
+              })}
+              placeholder='Password'
+              className='border tracking-wide mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
+            />
+            <span onClick={() => setShowPassword(!showPassword)} className='absolute right-3 text-lg top-3 text-[#2b2b2c] hover:text-[#121213] cursor-pointer'>
+              {
+                showPassword ? <FaEye /> : <FaEyeSlash />
+              }
+            </span>
+          </div>
+          {errors.password?.type === 'required' && <p className='text-red-500'>password is required!</p>}
+          {errors.password?.type === 'minLength' && <p className='text-red-500'>password must be 6 charecter or long!</p>}
         </div>
         <PrimaryButton widthFull={true}>
           Register
--- src/pages/Authentication/Signin/Signin.jsx
+++ src/pages/Authentication/Signin/Signin.jsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PrimaryButton from '../../../components/PrimaryButton';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { Link } from 'react-router';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Signin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  }
+
   return (
     <section className='max-w-sm'>
       <h2 className='text-4xl font-bold mb-3'>Welcome Back</h2>
       <p>Login with ZapShift</p>
-      <form className='mt-4 space-y-3'>
+      <form onSubmit={handleSubmit(handleLogin)} className='mt-4 space-y-3'>
         {/* email */}
         <div>
           <label htmlFor='email' className='text-[#0F172AFF]'>Email *</label>
           <input
             type='email'
-            name='email'
+            {...register('email', {
+              required: true
+            })}
             placeholder='Email'
             className='border mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
           />
+          {errors.email?.type === 'required' && <p className='text-red-500'>email is required!</p>}
         </div>
         {/* password */}
         <div>
           <label htmlFor='password' className='text-[#0F172AFF]'>Password *</label>
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            className='border mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
-          />
+          <div className='relative'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              {...register('password', {
+                required: true,
+                minLength: 6
+              })}
+              placeholder='Password'
+              className='border tracking-wide mt-1 text-[#94A3B8] border-[#94A3B8] px-4 py-2 outline-none block rounded-md w-full'
+            />
+            <span onClick={() => setShowPassword(!showPassword)} className='absolute right-3 text-lg top-3 text-[#2b2b2c] hover:text-[#121213] cursor-pointer'>
+              {
+                showPassword ? <FaEye /> : <FaEyeSlash />
+              }
+            </span>
+          </div>
+          {errors.password?.type === 'required' && <p className='text-red-500'>password is required!</p>}
+          {errors.password?.type === 'minLength' && <p className='text-red-500'>password must be 6 charecter or longer!</p>}
         </div>
         <PrimaryButton widthFull={true}>
           Sign In
